7d39f2b5d5388a53b3d01b6af453678983df1c75
HYK424 <turtle424@naver.com>
2021-08-15 21:48:57 +0900

    .

---
+++ src/main/java/com/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.book.springboot.domain.posts;
+
+//PostsRepository
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
