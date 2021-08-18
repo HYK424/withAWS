8b43fb99f7710a06c03b30c6a6c04dce7d7ce086
HYK424 <turtle424@naver.com>
2021-08-18 23:14:48 +0900

    index.mustache와 매핑/ 요청

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -12,12 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     private final PostsService postsService;
 
-    @GetMapping("/") //index.mustache에 url매핑. "/"를 요청하면 index가 나온다
+    @GetMapping("/") //index.mustache에 url매핑. "/"를 요청하면 index가 나온다.
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     } //컨트롤러 단에서 index이름을 쓰는 파일을 자동으로 찾아서 호출. resource/templates디렉토리 먼저 찾음
-
 
     @GetMapping("/posts/save")
     public String postsSave(){
