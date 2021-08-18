2c0b835adf539f135c85e58589143dedf7b4fc51
HYK424 <turtle424@naver.com>
2021-08-18 22:53:00 +0900

    SpringJPA의 기능을 사용하기 위한 인터페이스

--- src/main/java/com/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/book/springboot/domain/posts/PostsRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 
-public interface PostsRepository extends JpaRepository<Posts,Long> {
+public interface PostsRepository extends JpaRepository<Posts,Long> { //ctril+왼클릭 으로 해당 jpa의 기능 참조할 수 있다.
+    /*해당 인터페이스가 JpaRepository<>를 상속한다->해당 인터페이스는 JpaSpring의 기능들을 상속한다*/
 
-    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") //JPA의 기능. 쿼리 어노테이션을 붙여-> 메소드 이름으로 쿼리 생성
     List<Posts> findAllDesc();
 
 }
