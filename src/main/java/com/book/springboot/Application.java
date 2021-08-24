3b57879c82badf3f2a811afe0e69b7731e188f00
HYK424 <turtle424@naver.com>
2021-08-24 21:21:42 +0900

    메인 클래스

--- src/main/java/com/book/springboot/Application.java
+++ src/main/java/com/book/springboot/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 /*메인 클래스. 가장 위에서 위치하며 하위의 클래스들을 실행시킨다.*/
 
